32001f5953da379c7b55516816247fa4538183fb
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-24 08:26:15 +0200

    + MZ check

--- pe_checker/core.c
+++ pe_checker/core.c
@@ -11,5 +11,32 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+#pragma region "MZ magic number check"
+
+    FILE *fichier = fopen(argv[1], "rb");
+    if (fichier == NULL)
+    {
+        printf("Erreur d'ouverture du fichier\n");
+        return 1;
+    }
+
+    unsigned short magic_number;
+    fread(&magic_number, sizeof(unsigned short), 1, fichier); // buffer de destination, taille du buffer, nombre d'éléments à lire (1 unsigned short), fichier source
+
+    if (magic_number != 0x5A4D)
+    {
+        printf("Erreur : le fichier n'est pas un fichier PE valide\n");
+        fclose(fichier);
+        return 1;
+    }
+
+    printf("PE valide : 0x%X\n", magic_number);
+
+    fclose(fichier);
+
+#pragma endregion
+
+    printf("Appuyez sur une touche pour continuer...");
+    getchar();
     return 0;
 }
